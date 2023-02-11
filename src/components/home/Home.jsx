15d319e75463a233e135eed0c63d57bd5def980f
koushik22roy <koushik22dev@gmail.com>
2023-02-12 00:14:34 +0530

    setup gh-pages

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./home.css";
+import Socials from "./Socials";
+import Data from "./Data";
+const Home = () => {
+    return (
+        <section className="home section" id="home">
+            <div className="home__container container grid">
+                <div className="home__content grid">
+                    <Socials />
+                        <div className="home__img"></div>
+                    <Data />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Home
---
+++ src/components/home/Socials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Socials() {
+    return (
+        <div className="home__social">
+            <a href="#https://www.linkedin.com/in/koushik22roy/" className="home__social-icon" target='_blank'><i class="uil uil-linkedin"></i></a>
+            <a href="#https://github.com/" className="home__social-icon" target='_blank'><i class="uil uil-github-alt"></i></a>
+            <a href="#https://www.instagram.com/gama16r/" className="home__social-icon" target='_blank'><i class="uil uil-instagram-alt"></i></a>
+            <a href="#https://www.youtube.com/channel/UC5RZjjCN-meceDFWAaeefrw" className="home__social-icon" target='_blank'><i class="uil uil-youtube"></i></a>
+        </div>
+    )
+}
+
+export default Socials
