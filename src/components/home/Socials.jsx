582dc85a7f7ad4ac1f818d47b875ba3d9b0ee150
koushik22roy <koushik22dev@gmail.com>
2023-06-09 22:00:46 +0530

    added playstore icon and update the website

--- src/components/home/Socials.jsx
+++ src/components/home/Socials.jsx
@@ -5,6 +5,7 @@ function Socials() {
         <div className="home__socials center">
             <a href="https://www.linkedin.com/in/koushik22roy/" className="home__social-icon" target='_blank'><i class="uil uil-linkedin-alt"></i></a>
             <a href="https://github.com/koushik22roy" className="home__social-icon" target='_blank'><i class="uil uil-github-alt"></i></a>
+            <a href="https://play.google.com/store/apps/dev?id=8531115585235906226" className="home__social-icon" target='_blank'><i class="uil uil-google-play"></i></a>
             <a href="https://www.instagram.com/gama16r/" className="home__social-icon" target='_blank'><i class="uil uil-instagram"></i></a>
             <a href="https://twitter.com/gama16r" className="home__social-icon" target='_blank'><i class="uil uil-twitter-alt"></i></a>
             <a href="https://www.youtube.com/channel/UC5RZjjCN-meceDFWAaeefrw" className="home__social-icon" target='_blank'><i class="uil uil-youtube"></i></a>
