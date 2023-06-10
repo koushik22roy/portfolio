84a0ab9f45110cad59cff5d8dc5e94f184eb2374
koushik22roy <koushik22dev@gmail.com>
2023-06-10 10:49:41 +0530

    add facebook

--- src/components/home/Socials.jsx
+++ src/components/home/Socials.jsx
@@ -8,6 +8,7 @@ function Socials() {
             <a href="https://play.google.com/store/apps/dev?id=8531115585235906226" className="home__social-icon" target='_blank'><i class="uil uil-google-play"></i></a>
             <a href="https://www.instagram.com/gama16r/" className="home__social-icon" target='_blank'><i class="uil uil-instagram"></i></a>
             <a href="https://twitter.com/gama16r" className="home__social-icon" target='_blank'><i class="uil uil-twitter-alt"></i></a>
+            <a href="https://www.facebook.com/profile.php?id=100021841546445" className="home__social-icon" target='_blank'><i class="uil uil-facebook"></i></a>
             <a href="https://www.youtube.com/channel/UC5RZjjCN-meceDFWAaeefrw" className="home__social-icon" target='_blank'><i class="uil uil-youtube"></i></a>
         </div>
     )
